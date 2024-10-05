a1c2ee16accbd073b4f3cb57ab50906fd88627ce
Piz20 <eminiantpisani@gmail.com>
2024-10-05 09:44:49 +0100

    Ajout de l'option buy me a coffee...

--- lib/ui/developper_info_page.dart
+++ lib/ui/developper_info_page.dart
@@ -1,11 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class DeveloperInfoPage extends StatelessWidget {
+  const DeveloperInfoPage({super.key});
+
+  void _openPaymentLink(BuildContext context) async {
+    const url = 'https://nokash.co/pay-now/dl-67093aa4d01dd1728658';
+    if (await canLaunchUrl(Uri.parse(url))) {
+      await launchUrl(Uri.parse(url));
+    } else {
+      final controller = WebViewController()
+        ..setJavaScriptMode(JavaScriptMode.unrestricted)
+        ..setNavigationDelegate(
+          NavigationDelegate(
+            onProgress: (int progress) {
+              // You can implement a loading indicator here if needed
+            },
+            onPageStarted: (String url) {},
+            onPageFinished: (String url) {},
+            onWebResourceError: (WebResourceError error) {},
+            onNavigationRequest: (NavigationRequest request) {
+              // You can add navigation controls here if needed
+              return NavigationDecision.navigate;
+            },
+          ),
+        )
+        ..loadRequest(Uri.parse(url));
+
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => Scaffold(
+            appBar: AppBar(title: const Text('')),
+            body: WebViewWidget(controller: controller),
+          ),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Developers & App Info'),
+        title: const Text('Developers & App Info'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -13,46 +51,64 @@ class DeveloperInfoPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // App Developer Information
-            Text(
+            const Text(
               'Developers:',
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 8),
-            Text(
+            const Text(
               '• Piz',
               style: TextStyle(fontSize: 18),
             ),
             const SizedBox(height: 4),
-            Text(
+            const Text(
               '• Darrel',
               style: TextStyle(fontSize: 18),
             ),
             const SizedBox(height: 20),
-            // Purpose of the App
-            Text(
+            const Text(
               'This app was developed as part of the NASA Space Apps Challenge. The main objective of the app is to provide critical climate and natural disaster information such as temperature, flood risks, and other environmental hazards for specific regions. The data helps users make informed decisions and stay safe in case of natural disasters.',
               style: TextStyle(fontSize: 16),
             ),
             const SizedBox(height: 20),
-            // NASA Space Apps Challenge
-            Text(
+            const Text(
               'About NASA Space Apps Challenge:',
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 8),
-            Text(
+            const Text(
               'NASA Space Apps Challenge is an international hackathon that encourages innovative solutions to real-world problems faced by Earth and space sciences using open data provided by NASA.',
               style: TextStyle(fontSize: 16),
             ),
             const SizedBox(height: 20),
-            // Additional credits or acknowledgment (if necessary)
             Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context); // Return to the previous page
-                },
-                child: const Text('Back' , style: TextStyle(color: Colors.blue),),
+              child: Column(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(200, 40),
+                      backgroundColor: Colors.transparent,
+                      side: const BorderSide(color: Colors.blue),
+                    ),
+                    child: const Text('Back',
+                        style: TextStyle(color: Colors.blue)),
+                  ),
+                  const SizedBox(height: 16),
+                  ElevatedButton.icon(
+                    onPressed: () => _openPaymentLink(context),
+                    icon: const Icon(Icons.local_cafe, color: Colors.brown),
+                    label: const Text('Buy Me a Coffee',
+                        style: TextStyle(color: Colors.brown)),
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(200, 40),
+                      backgroundColor: Colors.transparent,
+                      side: const BorderSide(color: Colors.brown),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
